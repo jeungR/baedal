4653ea7f9968f1227ba6d98578fe358935220a11
Administrator <Administrator@MSDN-SPECIAL>
2018-05-28 18:54:15 +0900

    dto

---
+++ Baedal/src/com/bigdata/DTO/MenuDTO.java
@@ -0,0 +1,39 @@
+package com.bigdata.DTO;
+
+public class MenuDTO {
+	String order_code;
+	String food_code;
+	String number;
+	
+
+
+	public MenuDTO() {
+		super();
+	}
+
+	public String getOrder_code() {
+		return order_code;
+	}
+
+	public void setOrder_code(String order_code) {
+		this.order_code = order_code;
+	}
+
+	public String getFood_code() {
+		return food_code;
+	}
+
+	public void setFood_code(String food_code) {
+		this.food_code = food_code;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	
+}
---
+++ Baedal/src/com/bigdata/DTO/OrderDTO.java
@@ -0,0 +1,95 @@
+package com.bigdata.DTO;
+
+public class OrderDTO {
+	String code;
+	String totalprice;
+	String time;
+	String address;
+	String startdate;
+	String enddate;
+	String payment;
+	String ok;
+	String customer_code;
+	
+
+	public OrderDTO() {
+		super();
+	}
+
+	
+	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getTotaltipprice() {
+		return totalprice;
+	}
+
+	public void setTotaltipprice(String totaltipprice) {
+		this.totalprice = totaltipprice;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getStartdate() {
+		return startdate;
+	}
+
+	public void setStartdate(String startdate) {
+		this.startdate = startdate;
+	}
+
+	public String getEnddate() {
+		return enddate;
+	}
+
+	public void setEnddate(String enddate) {
+		this.enddate = enddate;
+	}
+
+	public String getPayment() {
+		return payment;
+	}
+
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+
+	public String getOk() {
+		return ok;
+	}
+
+	public void setOk(String ok) {
+		this.ok = ok;
+	}
+
+	public String getCustomer_code() {
+		return customer_code;
+	}
+
+	public void setCustomer_code(String customer_code) {
+		this.customer_code = customer_code;
+	}
+
+	
+	
+}
