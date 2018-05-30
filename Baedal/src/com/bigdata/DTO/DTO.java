2640157a82f74bc4f9198fe1903fab7348c0f9a6
SunAe <hsa9612@gmail.com>
2018-05-30 15:12:46 +0900

    Class 정리

--- Baedal/src/com/bigdata/DTO/DTO.java
+++
@@ -1,5 +0,0 @@
-package com.bigdata.DTO;
-
-public class DTO {
-
-}
--- Baedal/src/com/bigdata/command/CustomerUpdate_CustomerSelectCommnad.java
+++ Baedal/src/com/bigdata/command/CustomerUpdate_CustomerSelectCommnad.java
@@ -1,12 +1,9 @@
 package com.bigdata.command;
 
-import java.util.ArrayList;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.bigdata.DAO.CustomerUpdateDAO;
-import com.bigdata.DAO.MemberCustomerDAO;
 import com.bigdata.DTO.CustomerDTO;
 
 public class CustomerUpdate_CustomerSelectCommnad implements Command {
@@ -18,7 +15,7 @@ public class CustomerUpdate_CustomerSelectCommnad implements Command {
 		
 		CustomerUpdateDAO cDao = new CustomerUpdateDAO(); //db연결
 	//	ArrayList<CustomerDTO> dtos = cDao.list(custid);
-		CustomerDTO dto = cDao.UserInfoSearch(custid);
+		CustomerDTO dto = cDao.CustomerSelect(custid);
 	
 		
 	//	request.setAttribute("list", dtos);
