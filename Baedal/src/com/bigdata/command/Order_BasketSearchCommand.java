ebcf9fc6aa35e327d16f628ae637848e728e053b
SunAe <hsa9612@gmail.com>
2018-05-30 10:47:50 +0900

    커맨드 결합

--- Baedal/src/com/bigdata/command/Order_BasketSearchCommand.java
+++ Baedal/src/com/bigdata/command/Order_BasketSearchCommand.java
@@ -21,7 +21,12 @@ public class Order_BasketSearchCommand implements Command {
 		OrderDAO orderDAO = new OrderDAO();//데이터베이스 어세스(연결)
 		ArrayList<BasketDTO> dtos = orderDAO.BasketSearch(customer_code); //읽은 데이터를 배열로 저장
 		
+		CustomerDTO dto = orderDAO.UserInfoSearch(customer_code);
+		String mobile = dto.getMobile();
+		String address = dto.getAddress();
+		
 		request.setAttribute("BasketSearch", dtos); //리턴값 설정
+		request.setAttribute("Order", dto);
 	}
-
+			
 }
